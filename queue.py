b77940eaac8d21cd5c97299c9c7242e8d72f8291
benjamind10 <benjamind10@pm.me>
2024-06-05 15:10:04 -0400

    first commit

---
+++ queue.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_stack(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
